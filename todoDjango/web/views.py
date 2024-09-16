5eec84bdd24fdf3d0d61ac17f6086a0dbe811f74
overcodeme <placmabadei1985@rambler.ru>
2024-09-16 21:03:39 +0300

    - Working with view request

--- todoDjango/web/views.py
+++ todoDjango/web/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    host = request.META['HTTP_HOST']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path = request.path
+
+    return HttpResponse(f'''
+        <p>Host: {host}</p>
+        <p>Path: {path}</p>
+        <p>User-agent: {user_agent}</p>  
+    ''')
+
+
+def contacts(request):
+    return HttpResponse('<h1>Контакты</h1>')
